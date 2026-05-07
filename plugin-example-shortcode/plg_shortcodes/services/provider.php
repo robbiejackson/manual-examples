dbc235f56c92d0eb485cdfe4fc8646320c0301bb
Robbie Jackson <robbie.jackson@btinternet.com>
2026-05-07 19:39:59 +0100

    version separation removed, and note added to README

--- plugin-example-shortcode/plg_shortcodes/services/provider.php
+++ plugin-example-shortcode/plg_shortcodes/services/provider.php
@@ -12,53 +12,18 @@ use My\Plugin\Content\Shortcodes\Extension\Shortcode;
     {
         public function register(Container $container)
         {
-            if (version_compare(JVERSION, '6.1', '>=')) {
-                // Use lazy plugin class for performance 
-                // See https://github.com/joomla/joomla-cms/pull/45062
-                $container->set(
-                    PluginInterface::class,
-                    $container->lazy(Shortcode::class, function (Container $container) {
-        
-                        $config = (array) PluginHelper::getPlugin('content', 'shortcodes');
-                        $app = Factory::getApplication();
-                        
-                        $plugin = new Shortcode($config);
-                        $plugin->setApplication($app);
-        
-                        return $plugin;
-                    })
-                );
-            } else if (version_compare(JVERSION, '5.3', '>=')) {
-                // Dispatcher passed to plugin constructor deprecated 
-                // See https://github.com/joomla/joomla-cms/pull/43430
-                $container->set(
-                    PluginInterface::class,
-                    function (Container $container) {
-        
-                        $config = (array) PluginHelper::getPlugin('content', 'shortcodes');
-                        $app = Factory::getApplication();
-                        
-                        $plugin = new Shortcode($config);
-                        $plugin->setApplication($app);
-        
-                        return $plugin;
-                    }
-                );
-            } else {
-                $container->set(
-                    PluginInterface::class,
-                    function (Container $container) {
-        
-                        $config = (array) PluginHelper::getPlugin('content', 'shortcodes');
-                        $dispatcher = $container->get(DispatcherInterface::class);
-                        $app = Factory::getApplication();
-                        
-                        $plugin = new Shortcode($dispatcher, $config);
-                        $plugin->setApplication($app);
-        
-                        return $plugin;
-                    }
-                );
-            }
+            $container->set(
+                PluginInterface::class,
+                $container->lazy(Shortcode::class, function (Container $container) {
+    
+                    $config = (array) PluginHelper::getPlugin('content', 'shortcodes');
+                    $app = Factory::getApplication();
+                    
+                    $plugin = new Shortcode($config);
+                    $plugin->setApplication($app);
+    
+                    return $plugin;
+                })
+            );
         }
     };
